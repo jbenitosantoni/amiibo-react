749ba4a59a33b5b83c626b5ed0451b99122b45e0
Javier Benito <javierbenitosantoni@icloud.com>
2020-07-06 20:29:20 +0200

    Buscador

--- src/components/pages/amiibos/Amiibos.js
+++ src/components/pages/amiibos/Amiibos.js
@@ -1,15 +1,58 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Gallery} from "./components/gallery/Gallery";
+import {SearchBar} from "./components/searchbar/Searchbar";
 
 export function Amiibos() {
-    const [amiibos, setAmiibo] = useState(0);
+    const [amiibos, setAmiibo] = useState([]);
+    const [filteredAmiibos, setFilteredAmiibos] = useState([]);
     useEffect(() => {
         axios.get('https://www.amiiboapi.com/api/amiibo/').then(res => {
             setAmiibo(res.data.amiibo);
+            setFilteredAmiibos(res.data.amiibo);
         })
     }, []);
+    const filterAmiibos = (filterValues) => {
+        let arrayAmiibos = [];
+        for (let i = 0; i < amiibos.length; i++) {
+            if (amiibos[i].character === filterValues.amiiboName && amiibos[i].type === filterValues.type && amiibos[i].gameSeries === filterValues.gameSeries) {
+                arrayAmiibos.push(amiibos[i]);
+            }
+            if (amiibos[i].character === filterValues.amiiboName && amiibos[i].gameSeries === filterValues.gameSeries && filterValues.type === "") {
+                arrayAmiibos.push(amiibos[i]);
+            }
+            if (amiibos[i].character === filterValues.amiiboName && filterValues.gameSeries === "" && amiibos[i].type === filterValues.type) {
+                arrayAmiibos.push(amiibos[i]);
+            }
+            if (filterValues.amiiboName === "" && amiibos[i].gameSeries === filterValues.gameSeries && filterValues.type === "") {
+                arrayAmiibos.push(amiibos[i]);
+            }
+            if (filterValues.amiiboName === "" && filterValues.gameSeries === "" && amiibos[i].type === filterValues.type) {
+                arrayAmiibos.push(amiibos[i]);
+            }
+            if (filterValues.gameSeries === amiibos[i].gameSeries && filterValues.amiiboName === "" && filterValues.type === "") {
+                arrayAmiibos.push(amiibos[i]);
+            }
+            if (filterValues.gameSeries === amiibos[i].gameSeries && filterValues.amiiboName === "" && filterValues.type === amiibos[i].type) {
+                arrayAmiibos.push(amiibos[i]);
+            }
+            if (filterValues.type === amiibos[i].type && filterValues.amiiboName === amiibos[i].character && filterValues.gameSeries === "") {
+                arrayAmiibos.push(amiibos[i]);
+            }
+            if (filterValues.type === amiibos[i].type && filterValues.amiiboName === "" && filterValues.gameSeries === amiibos[i].gameSeries) {
+                arrayAmiibos.push(amiibos[i]);
+            }
+            if (filterValues.character === amiibos[i].amiiboName && filterValues.gameSeries === "" && filterValues.type === "") {
+                arrayAmiibos.push(amiibos[i]);
+            }
+        }
+        setFilteredAmiibos(arrayAmiibos);
+    }
+
     return (
-        <Gallery props={amiibos}/>
+        <div>
+            <SearchBar props={amiibos} fnSubmit={filterAmiibos}/><br/>
+            <Gallery props={filteredAmiibos}/>
+        </div>
     );
 }
--- src/components/pages/amiibos/components/searchbar/Searchbar.js
+++ src/components/pages/amiibos/components/searchbar/Searchbar.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 
 export function SearchBar(props) {
+    let gameSeries = [];
+    let options = [];
+    const {register, handleSubmit, errors} = useForm();
+    const onSubmit = data => props.fnSubmit(data);
+    console.log(errors);
+
+    for (let i = 0; i < props.props.length; i++) {
+        gameSeries.push(props.props[i].gameSeries);
+    }
+
+    gameSeries = [...new Set(gameSeries)];
+
+    for (let i = 0; i < gameSeries.length; i++) {
+        options.push(<option value={gameSeries[i]}>{gameSeries[i]}</option>);
+    }
 
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <input type="text" placeholder="Amiibo Name" name="amiiboName" ref={register}/>
+            </div>
+            <div>
+                <select name="gameSeries" ref={register}>
+                    <option value=""></option>
+                    {options}
+                </select>
+            </div>
+            <div>
+                <select name="type" ref={register}>
+                    <option value=""></option>
+                    <option value="Figure">Figure</option>
+                    <option value="Card">Card</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit"/>
+            </div>
+        </form>
     );
 }
