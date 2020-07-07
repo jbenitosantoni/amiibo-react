2165332589d7f0e23f4f16116b77631d919c15f8
Javier Benito <javierbenitosantoni@icloud.com>
2020-07-07 10:38:39 +0200

    Searchbar Improved

--- src/components/pages/amiibos/Amiibos.js
+++ src/components/pages/amiibos/Amiibos.js
@@ -12,40 +12,36 @@ export function Amiibos() {
             setFilteredAmiibos(res.data.amiibo);
         })
     }, []);
+
     const filterAmiibos = (filterValues) => {
         let arrayAmiibos = [];
+
+        let consulta = [];
+
+        if (filterValues.amiiboName === "") {
+            consulta.push('filterValues.amiiboName === ""');
+        } else {
+            consulta.push("amiibos[i].character === filterValues.amiiboName");
+        }
+        if (filterValues.gameSeries === "") {
+            consulta.push('filterValues.gameSeries === ""');
+        } else {
+            consulta.push("amiibos[i].gameSeries === filterValues.gameSeries");
+        }
+        if (filterValues.type === "") {
+            consulta.push('filterValues.type === ""');
+        } else {
+            consulta.push("amiibos[i].type === filterValues.type");
+        }
+
+        consulta = consulta.join(" && ");
+
         for (let i = 0; i < amiibos.length; i++) {
-            if (amiibos[i].character === filterValues.amiiboName && amiibos[i].type === filterValues.type && amiibos[i].gameSeries === filterValues.gameSeries) {
-                arrayAmiibos.push(amiibos[i]);
-            }
-            if (amiibos[i].character === filterValues.amiiboName && amiibos[i].gameSeries === filterValues.gameSeries && filterValues.type === "") {
-                arrayAmiibos.push(amiibos[i]);
-            }
-            if (amiibos[i].character === filterValues.amiiboName && filterValues.gameSeries === "" && amiibos[i].type === filterValues.type) {
-                arrayAmiibos.push(amiibos[i]);
-            }
-            if (filterValues.amiiboName === "" && amiibos[i].gameSeries === filterValues.gameSeries && filterValues.type === "") {
-                arrayAmiibos.push(amiibos[i]);
-            }
-            if (filterValues.amiiboName === "" && filterValues.gameSeries === "" && amiibos[i].type === filterValues.type) {
-                arrayAmiibos.push(amiibos[i]);
-            }
-            if (filterValues.gameSeries === amiibos[i].gameSeries && filterValues.amiiboName === "" && filterValues.type === "") {
-                arrayAmiibos.push(amiibos[i]);
-            }
-            if (filterValues.gameSeries === amiibos[i].gameSeries && filterValues.amiiboName === "" && filterValues.type === amiibos[i].type) {
-                arrayAmiibos.push(amiibos[i]);
-            }
-            if (filterValues.type === amiibos[i].type && filterValues.amiiboName === amiibos[i].character && filterValues.gameSeries === "") {
-                arrayAmiibos.push(amiibos[i]);
-            }
-            if (filterValues.type === amiibos[i].type && filterValues.amiiboName === "" && filterValues.gameSeries === amiibos[i].gameSeries) {
-                arrayAmiibos.push(amiibos[i]);
-            }
-            if (filterValues.character === amiibos[i].amiiboName && filterValues.gameSeries === "" && filterValues.type === "") {
+            if (eval(consulta)) {
                 arrayAmiibos.push(amiibos[i]);
             }
         }
+
         setFilteredAmiibos(arrayAmiibos);
     }
 
